d402b603bdb59599edcac43436ab65f5dcfa85d7
Eugene Sandulenko <sev@scummvm.org>
2024-05-14 22:43:25 +0200

    Define custom data types for portability

--- src/common/str.h
+++ src/common/str.h
@@ -11,8 +11,12 @@
 #include <ostream>
 #include <string>
 
-typedef uint32_t uint32;
 typedef unsigned int uint;
+typedef uint8_t byte;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
 
 namespace Common {
 class String {
